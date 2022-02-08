9879c1444148b1d5e8df78f742baa9f8df8db1da
Mukhammad Akilov <muhammad_akilov@mail.ru>
2022-02-08 17:33:36 +0500

    Add: React Router v6, custom hooks Change: convert all components to TypeScript components, connect Material UI with TypeScript. Buf fixing: fixed several bugs

---
+++ src/store-deprecated/store.js
@@ -0,0 +1,22 @@
+import {createStore, applyMiddleware} from "redux";
+import thunk from "redux-thunk";
+import {composeWithDevTools} from "redux-devtools-extension";
+import rootReducer from "./rootReducer";
+import {removeAuthDataFromLocalStorage} from "../components/Utils/utils";
+
+const middlewares = [thunk];
+
+const store = createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(...middlewares))
+);
+
+store.subscribe(() => {
+    const state = store.getState();
+    if(!state.user.isAuth) {
+        removeAuthDataFromLocalStorage();
+    }
+});
+
+
+export default store;
